6b7fd5a884d02c616f2938cd9c4331eaba7a0114
xtophe xanon <xocon10@gmail.com>
2020-07-29 13:51:55 -0500

    create server and initial html routes

---
+++ server.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+
+const PORT = process.env.PORT || 3001;
+
+app.use(express.static('public'));
+
+app.get('/notes', (req, res) => {
+	res.sendFile(path.join(__dirname, './public/notes.html'));
+});
+
+app.get('/', (req, res) => {
+	res.sendFile(path.join(__dirname, './public/index.html'));
+});
+
+app.get('*', (req, res) => {
+	res.sendFile(path.join(__dirname, './public/index.html'));
+});
+
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
